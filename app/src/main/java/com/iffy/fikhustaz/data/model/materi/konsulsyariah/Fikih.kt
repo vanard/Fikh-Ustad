06c294dac4abb00038cb05185dd6ee8ee70a38dc
Vian RD <vianixa34@gmail.com>
2020-10-29 17:04:28 +0800

    update model

--- app/src/main/java/com/iffy/fikhustaz/data/model/materi/konsulsyariah/Fikih.kt
+++ app/src/main/java/com/iffy/fikhustaz/data/model/materi/konsulsyariah/Fikih.kt
@@ -4,6 +4,13 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Fikih (val title: String, val link: String, val dateTime: String, val date: String, val choose: String) : Parcelable {
-    constructor(): this ("","","","","")
+data class Fikih (
+    val title: String,
+    val link: String,
+    val dateTime: String,
+    val date: String,
+    val choose: String,
+    val thumb: String
+) : Parcelable {
+    constructor(): this ("","","","","", "")
 }
